16b0c906806551d5414a15a54585614987fc4023
Jing Luo <szmun.luoj@gmail.com>
2023-04-09 16:47:19 +0900

    Initial commit
    Signed-off-by: Jing Luo <szmun.luoj@gmail.com>

---
+++ main.js
@@ -0,0 +1,37 @@
+/* Author: Jing Luo, Mar 2023
+ * Email: szmun.luoj@gmail.com
+ */
+
+const electron = require ('electron')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+
+let win
+if (require('electron-squirrel-startup')) app.quit();
+
+app.on('ready', _ => {
+    win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    })
+    const template = [
+        {
+            label: 'Quit',
+            role: 'quit'
+        },
+        {
+            label: 'Refresh',
+            role: 'reload'
+        }
+    ]
+    const menu = Menu.buildFromTemplate (template)
+    Menu.setApplicationMenu (menu)
+
+    win.loadURL('https://mybb.jing.rocks')
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
