ebb836acd98ab5e37cecd88cc953c9bee766d098
Jing Luo <szmun.luoj@gmail.com>
2023-07-10 16:32:12 +0900

    Update to mitigate some security vulnerabilities
    Signed-off-by: Jing Luo <szmun.luoj@gmail.com>

--- forge.config.js
+++ forge.config.js
@@ -15,20 +15,6 @@ module.exports = {
       name: '@electron-forge/maker-zip',
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {
-        options: {
-            icon: 'favicon.png',  // Path to a single image that will act as icon for the application
-          }
-      },
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {
-        icon: 'favicon.png',
-      },
-    },
-    {
       name: '@electron-forge/maker-dmg',
       config: {
         icon: 'favicon.icns',  // Path to the icon to use for the app in the DMG window
@@ -39,16 +25,6 @@ module.exports = {
       config: {
         icon: 'favicon.ico',
       },
-    },
-    {
-      name: '@electron-forge/maker-snap',
-      config: {
-        features: {
-          audio: true,
-          webgl: true,
-        },
-        summary: 'Eden\'s snap package',
-      }
     },
   ],
 };
